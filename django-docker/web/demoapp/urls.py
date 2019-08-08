5f14265b2057fe6a585195c8d390df0bc5ccf9f4
root <root@ip-172-31-41-217.ap-northeast-2.compute.internal>
2019-08-08 05:09:41 +0000

    extra

--- django-docker/web/demoapp/urls.py
+++ django-docker/web/demoapp/urls.py
@@ -5,8 +5,5 @@ app_name = 'demoapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('celery/', views.celery_index, name='celery_index'),
-    path('celery/random_add/', views.random_add, name='celery_random_add'),
-    path('celery/random_mul/', views.random_mul, name='celery_random_mul'),
-    path('celery/random_xsum/', views.random_xsum, name='celery_random_xsum'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
