c3df0502196d2d494f4259adf4c3b24ee2be13f5
bjonathan1 <32161963+bjonathan1@users.noreply.github.com>
2019-08-10 15:20:49 +0900

    Local 작업환경을 위한 수정

--- django-docker/web/demoapp/views.py
+++ django-docker/web/demoapp/views.py
@@ -1,12 +1,12 @@
-import random
-from django.shortcuts import render
 from . import tasks
+from django.shortcuts import render
+
 
 
 def index(request):
     context = {}
-    return render(request, 'demoapp/index.html', context)
+    return render(request, 'index.html', context)
 
 def dashboard(request):
     context = {}
-    return render(request, 'demoapp/dashboard.html', context)
+    return render(request, 'dashboard.html', context)
