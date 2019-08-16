9886035c856b268ec7f7863107a3662d02e48d82
root <root@ip-172-31-41-217.ap-northeast-2.compute.internal>
2019-08-16 05:14:01 +0000

    meeting 회의 생성 팝업

--- django-docker/web/staticfiles/assets/js/calendar/fullcalendar.js
+++ django-docker/web/staticfiles/assets/js/calendar/fullcalendar.js
@@ -2383,7 +2383,7 @@ function BasicView(element, calendar, viewName) {
 			"<div>";
 
 		if (showNumbers) {
-			html += "<div class='fc-day-number'>" + date.getDate() + "</div>";
+			html += "<div data-toggle='modal' data-target='#eventcreateModal' class='fc-day-number'>" + date.getDate() + "</div>";
 		}
 
 		html +=
@@ -5953,7 +5953,7 @@ function OverlayManager() {
 		if (e[0].parentNode != parent[0]) {
 			e.appendTo(parent);
 		}
-		usedOverlays.push(e.css(rect).show());
+		
 		return e;
 	}
 	
@@ -6108,4 +6108,5 @@ function HorizontalPositionCache(getElement) {
 ;;
 
 })(jQuery);
+
 
