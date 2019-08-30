faefe29496c0f58477e09f6563c9184a2bc7a294
bjonathan1 <32161963+bjonathan1@users.noreply.github.com>
2019-08-30 09:20:19 +0900

    3줄 삭제

--- django-docker/web/demoapp/views.py
+++ django-docker/web/demoapp/views.py
@@ -129,9 +129,9 @@ def meeting(request):
     try:
         data2 = {'apiId': 'skku-api-id', 'apiKey': '5fe8577c35784d07a840e0783bda56c7'}
 
-        link = storage.child('wav').child('files').get_url(1)
+        link = storage.child('wav').child('files.wav').get_url(1)
 
-        storage.child("wav").child('files').download("staticfiles/assets/wavfile/file.wav")
+        storage.child("wav").child('files.wav').download("staticfiles/assets/wavfile/file.wav")
 
         response = urllib.request.urlopen(link)
         print(response)
@@ -334,155 +334,155 @@ def meeting(request):
         q.close()
 
         ## 화자 분리 파일 저장
-
-        ## 형태소 분석해서 키워드와 주요문장 뽑아내기
-        class SentenceTokenizer(object):
-
-            def __init__(self):
-                self.kkma = Kkma()
-                self.twitter = Twitter()
-                self.stopwords = ["speaker", "백화점", "롯데", "아", "휴", "아이구", "아이쿠", "아이고", ]
-
-            def url2sentences(self, url):
-                article = Article(url, language='ko')
-                article.download()
-                article.parse()
-                sentences = self.kkma.sentences(article.text)
-
-                for idx in range(0, len(sentences)):
-                    if len(sentences[idx]) <= 10:
-                        sentences[idx - 1] += (' ' + sentences[idx])
-                        sentences[idx] = ''
-
-                return sentences
-
-            def txt2sentences(self, txt):
-                text = text(txt, language='ko')
-                text.download()
-                text.parse()
-                sentences = self.kkma.sentences(article.text)
-
-                for idx in range(0, len(sentences)):
-                    if len(sentences[idx]) <= 10:
-                        sentences[idx - 1] += (' ' + sentences[idx])
-                        sentences[idx] = ''
-
-                return sentences
-
-            def text2sentences(self, text):
-                sentences = self.kkma.sentences(text)
-                for idx in range(0, len(sentences)):
-                    if len(sentences[idx]) <= 10:
-                        sentences[idx - 1] += (' ' + sentences[idx])
-                        sentences[idx] = ''
-
-                return sentences
-
-            def get_nouns(self, sentences):
-                nouns = []
-                for sentence in sentences:
-                    if sentence != '':
-                        nouns.append(' '.join([noun for noun in self.twitter.nouns(str(sentence))
-                                               if noun not in self.stopwords and len(noun) > 1]))
-                return nouns
-
-        class GraphMatrix(object):
-            def __init__(self):
-                self.tfidf = TfidfVectorizer()
-                self.cnt_vec = CountVectorizer()
-                self.graph_sentence = []
-
-            def build_sent_graph(self, sentence):
-                tfidf_mat = self.tfidf.fit_transform(sentence).toarray()
-                self.graph_sentence = np.dot(tfidf_mat, tfidf_mat.T)
-                return self.graph_sentence
-
-            def build_words_graph(self, sentence):
-                cnt_vec_mat = normalize(self.cnt_vec.fit_transform(sentence).toarray().astype(float), axis=0)
-                vocab = self.cnt_vec.vocabulary_
-                return np.dot(cnt_vec_mat.T, cnt_vec_mat), {vocab[word]: word for word in vocab}
-
-        class Rank(object):
-            def get_ranks(self, graph, d=0.85):  # d = damping factor
-                A = graph
-                matrix_size = A.shape[0]
-                for id in range(matrix_size):
-                    A[id, id] = 0  # diagonal 부분을 0으로
-                    link_sum = np.sum(A[:, id])  # A[:, id] = A[:][id]
-                    if link_sum != 0:
-                        A[:, id] /= link_sum
-                    A[:, id] *= -d
-                    A[id, id] = 1
-                B = (1 - d) * np.ones((matrix_size, 1))
-                ranks = np.linalg.solve(A, B)  # 연립방정식 Ax = b
-                return {idx: r[0] for idx, r in enumerate(ranks)}
-
-        class TextRank(object):
-            def __init__(self, text):
-                self.sent_tokenize = SentenceTokenizer()
-                if text[:5] in ('http:', 'https'):
-                    self.sentences = self.sent_tokenize.url2sentences(text)
-                else:
-                    self.sentences = self.sent_tokenize.text2sentences(text)
-                self.nouns = self.sent_tokenize.get_nouns(self.sentences)
-                self.graph_matrix = GraphMatrix()
-                self.sent_graph = self.graph_matrix.build_sent_graph(self.nouns)
-                self.words_graph, self.idx2word = self.graph_matrix.build_words_graph(self.nouns)
-                self.rank = Rank()
-                self.sent_rank_idx = self.rank.get_ranks(self.sent_graph)
-                self.sorted_sent_rank_idx = sorted(self.sent_rank_idx, key=lambda k: self.sent_rank_idx[k], reverse=True)
-                self.word_rank_idx = self.rank.get_ranks(self.words_graph)
-                self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
-
-            def summarize(self, sent_num=1):
-                summary = []
-                index = []
-                for idx in self.sorted_sent_rank_idx[:sent_num]:
-                    index.append(idx)
-                index.sort()
-                for idx in index:
-                    summary.append(self.sentences[idx])
-                return summary
-
-            def keywords(self, word_num=3):
-                ## 워드 넘버로 키워드 갯수 조정
-                rank = Rank()
-                rank_idx = rank.get_ranks(self.words_graph)
-                sorted_rank_idx = sorted(rank_idx, key=lambda k: rank_idx[k], reverse=True)
-                keywords = []
-                index = []
-                for idx in sorted_rank_idx[:word_num]:
-                    index.append(idx)
-                # index.sort()
-                for idx in index:
-                    keywords.append(self.idx2word[idx])
-                return keywords
-
-        summary = ''
-        if(s):
-            textrank = TextRank(s)
-            for row in textrank.summarize(3):
-                ##summarize 넘버로 주요 문장 숫자 조정
-                ##print(row)
-                summary = summary + row + '\n'
-            ##print('keywords :',textrank.keywords())
-            keywords = ('keywords :', textrank.keywords())
-
-            summary = summary.replace('speaker0:', "")
-            summary = summary.replace('speaker1:', "")
-            summary = summary.replace('speaker2:', "")
-            summary = summary.replace('speaker3:', "")
-            summary = summary.replace('speaker4:', "")
-            summary = summary.replace('speaker5:', "")
-
-            final = open(r'staticfiles/assets/wavfile/textfile.txt', 'w')
-            final.write(s + '\n')
-            final.close()
+        #
+        # ## 형태소 분석해서 키워드와 주요문장 뽑아내기
+        # class SentenceTokenizer(object):
+        #
+        #     def __init__(self):
+        #         self.kkma = Kkma()
+        #         self.twitter = Twitter()
+        #         self.stopwords = ["speaker", "백화점", "롯데", "아", "휴", "아이구", "아이쿠", "아이고", ]
+        #
+        #     def url2sentences(self, url):
+        #         article = Article(url, language='ko')
+        #         article.download()
+        #         article.parse()
+        #         sentences = self.kkma.sentences(article.text)
+        #
+        #         for idx in range(0, len(sentences)):
+        #             if len(sentences[idx]) <= 10:
+        #                 sentences[idx - 1] += (' ' + sentences[idx])
+        #                 sentences[idx] = ''
+        #
+        #         return sentences
+        #
+        #     def txt2sentences(self, txt):
+        #         text = text(txt, language='ko')
+        #         text.download()
+        #         text.parse()
+        #         sentences = self.kkma.sentences(article.text)
+        #
+        #         for idx in range(0, len(sentences)):
+        #             if len(sentences[idx]) <= 10:
+        #                 sentences[idx - 1] += (' ' + sentences[idx])
+        #                 sentences[idx] = ''
+        #
+        #         return sentences
+        #
+        #     def text2sentences(self, text):
+        #         sentences = self.kkma.sentences(text)
+        #         for idx in range(0, len(sentences)):
+        #             if len(sentences[idx]) <= 10:
+        #                 sentences[idx - 1] += (' ' + sentences[idx])
+        #                 sentences[idx] = ''
+        #
+        #         return sentences
+        #
+        #     def get_nouns(self, sentences):
+        #         nouns = []
+        #         for sentence in sentences:
+        #             if sentence != '':
+        #                 nouns.append(' '.join([noun for noun in self.twitter.nouns(str(sentence))
+        #                                        if noun not in self.stopwords and len(noun) > 1]))
+        #         return nouns
+        #
+        # class GraphMatrix(object):
+        #     def __init__(self):
+        #         self.tfidf = TfidfVectorizer()
+        #         self.cnt_vec = CountVectorizer()
+        #         self.graph_sentence = []
+        #
+        #     def build_sent_graph(self, sentence):
+        #         tfidf_mat = self.tfidf.fit_transform(sentence).toarray()
+        #         self.graph_sentence = np.dot(tfidf_mat, tfidf_mat.T)
+        #         return self.graph_sentence
+        #
+        #     def build_words_graph(self, sentence):
+        #         cnt_vec_mat = normalize(self.cnt_vec.fit_transform(sentence).toarray().astype(float), axis=0)
+        #         vocab = self.cnt_vec.vocabulary_
+        #         return np.dot(cnt_vec_mat.T, cnt_vec_mat), {vocab[word]: word for word in vocab}
+        #
+        # class Rank(object):
+        #     def get_ranks(self, graph, d=0.85):  # d = damping factor
+        #         A = graph
+        #         matrix_size = A.shape[0]
+        #         for id in range(matrix_size):
+        #             A[id, id] = 0  # diagonal 부분을 0으로
+        #             link_sum = np.sum(A[:, id])  # A[:, id] = A[:][id]
+        #             if link_sum != 0:
+        #                 A[:, id] /= link_sum
+        #             A[:, id] *= -d
+        #             A[id, id] = 1
+        #         B = (1 - d) * np.ones((matrix_size, 1))
+        #         ranks = np.linalg.solve(A, B)  # 연립방정식 Ax = b
+        #         return {idx: r[0] for idx, r in enumerate(ranks)}
+        #
+        # class TextRank(object):
+        #     def __init__(self, text):
+        #         self.sent_tokenize = SentenceTokenizer()
+        #         if text[:5] in ('http:', 'https'):
+        #             self.sentences = self.sent_tokenize.url2sentences(text)
+        #         else:
+        #             self.sentences = self.sent_tokenize.text2sentences(text)
+        #         self.nouns = self.sent_tokenize.get_nouns(self.sentences)
+        #         self.graph_matrix = GraphMatrix()
+        #         self.sent_graph = self.graph_matrix.build_sent_graph(self.nouns)
+        #         self.words_graph, self.idx2word = self.graph_matrix.build_words_graph(self.nouns)
+        #         self.rank = Rank()
+        #         self.sent_rank_idx = self.rank.get_ranks(self.sent_graph)
+        #         self.sorted_sent_rank_idx = sorted(self.sent_rank_idx, key=lambda k: self.sent_rank_idx[k], reverse=True)
+        #         self.word_rank_idx = self.rank.get_ranks(self.words_graph)
+        #         self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
+        #
+        #     def summarize(self, sent_num=1):
+        #         summary = []
+        #         index = []
+        #         for idx in self.sorted_sent_rank_idx[:sent_num]:
+        #             index.append(idx)
+        #         index.sort()
+        #         for idx in index:
+        #             summary.append(self.sentences[idx])
+        #         return summary
+        #
+        #     def keywords(self, word_num=3):
+        #         ## 워드 넘버로 키워드 갯수 조정
+        #         rank = Rank()
+        #         rank_idx = rank.get_ranks(self.words_graph)
+        #         sorted_rank_idx = sorted(rank_idx, key=lambda k: rank_idx[k], reverse=True)
+        #         keywords = []
+        #         index = []
+        #         for idx in sorted_rank_idx[:word_num]:
+        #             index.append(idx)
+        #         # index.sort()
+        #         for idx in index:
+        #             keywords.append(self.idx2word[idx])
+        #         return keywords
+        #
+        # summary = ''
+        # if(s):
+        #     textrank = TextRank(s)
+        #     for row in textrank.summarize(3):
+        #         ##summarize 넘버로 주요 문장 숫자 조정
+        #         ##print(row)
+        #         summary = summary + row + '\n'
+        #     ##print('keywords :',textrank.keywords())
+        #     keywords = ('keywords :', textrank.keywords())
+        #
+        #     summary = summary.replace('speaker0:', "")
+        #     summary = summary.replace('speaker1:', "")
+        #     summary = summary.replace('speaker2:', "")
+        #     summary = summary.replace('speaker3:', "")
+        #     summary = summary.replace('speaker4:', "")
+        #     summary = summary.replace('speaker5:', "")
+        #
+        #     final = open(r'staticfiles/assets/wavfile/textfile.txt', 'w')
+        #     final.write(s + '\n')
+        #     final.close()
 
         os.remove(r'staticfiles/assets/wavfile/diarizationresult1.txt')
         os.remove(r'staticfiles/assets/wavfile/newSong.wav')
 
-        context["summary"] = summary
+        context["summary"] = s
     except AttributeError:
         context["summary"] = ""
     return render(request, 'demoapp/meeting.html', context)
